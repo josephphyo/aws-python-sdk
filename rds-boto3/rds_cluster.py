07a15d69c69dbee62fbe32c347c82f4d5047dc5c
Phyo Min Htun <htun@neozo.de>
2024-10-08 11:39:41 +0700

    chore: RDS DB Cluster Creation

---
+++ rds-boto3/rds_cluster.py
@@ -0,0 +1,45 @@
+import boto3
+import time
+
+rds = boto3.client('rds')
+
+username = 'RDS_ADMIN'
+password = 'RANDOM_PASS'
+db_subnet_group = 'rds-db-subnetgroup-name'
+db_cluster_id = 'rds-boto-hol'
+db_engine_mode = 'serverless'  # serverless for aurora serverless V1, V2 is provisioned.
+
+# --- create db cluster , check cluster is already exist or not.
+try:
+    response = rds.describe_db_clusters(DBClusterIdentifier=db_cluster_id)
+    print(f"The DB Cluster '{db_cluster_id}' already exist. ")
+except rds.exceptions.DBClusterNotFoundFault:
+    response = rds.create_db_cluster(
+        DBSubnetGroupName=db_subnet_group,
+        DBClusterIdentifier=db_cluster_id,
+        Engine='aurora-mysql',
+        EngineVersion='5.7.mysql_aurora.2.80.3',
+        MasterUsername=username,
+        MasterUserPassword=password,
+        EngineMode=db_engine_mode,
+        EnableHttpEndpoint=True,
+
+        ScalingConfiguration={
+            'MinCapacity': 1,
+            'MaxCapacity': 8,
+            'AutoPause': True,
+            'SecondsUntilAutoPause': 300  # Pause after 5 minutes IDLE. (Cost Saving)
+        }
+    )
+    print(f"The DB Cluster '{db_cluster_id}' Created")
+
+    # --- Wait for DB Cluster Available.
+    while True:
+        response = rds.describe_db_clusters(DBClusterIdentifier=db_cluster_id)
+        status = response['DBClusters'][0]['Status']
+        print(f"The status of the cluster is '{status}'")
+        if status == "available":
+            break
+
+        print("Waiting for the DB cluster to become available....")
+        time.sleep(40)
